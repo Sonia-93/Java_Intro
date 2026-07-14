a19251985ca1e372b3196cb81883e1cbc96180d1
Wateta <shimirwasonia5@gmail.com>
2026-07-15 00:12:16 +0200

    update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print ln("Hello world");
+        int a=10;
+        System.out.println(a);
+        final int b=20;
+        b=10; // This line will cause a compilation error because 'b' is declared as final and cannot be reassigned.
+        System.out.println(b);
+
     }
 }
  
