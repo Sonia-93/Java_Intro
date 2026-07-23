f9dba6c28c1532c360d398eb722b61c152944912
Wateta <shimirwasonia5@gmail.com>
2026-07-24 00:45:14 +0200

    update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,19 +2,10 @@
 class Computer{
 
     
-    public void playMusic(){
-      System.out.println("Music playing");
-    }
-    public String getMeApen(int cost){
-        return "Pen";
-    }
+   
 }
 
 public class Demo{
-    public static void  main(String a[]){
-      Computer obj=new Computer();
-      obj.playMusic();
-     String str= obj.getMeApen(100);
-     System.out.println(str);
+   
     }
 }
