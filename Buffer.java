803558de1001a295fb47a22611912291700e89f7
Wateta <shimirwasonia5@gmail.com>
2026-07-29 12:50:04 +0200

    update Buffer.java

--- Buffer.java
+++ Buffer.java
@@ -3,7 +3,7 @@ public class Buffer{
     
     public static void main(String[] args){
    StringBuffer sb=new StringBuffer("Hello");
-   System.out.println(sb.length());
+   System.out.println(sb.length()); 
         
     }
 }
