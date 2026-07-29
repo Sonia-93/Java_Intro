f2cc8944af8360c41cd5bfdba1dd9db61fdcc104
Wateta <shimirwasonia5@gmail.com>
2026-07-29 13:04:13 +0200

    update Buffer.class and Buffer.java

--- Buffer.java
+++ Buffer.java
@@ -4,7 +4,8 @@ public class Buffer{
     public static void main(String[] args){
    StringBuffer sb=new StringBuffer("Hello");
    System.out.println(sb.length()); 
-   
+   sb.append(" World");
+    System.out.println(sb);
         
     }
 }
