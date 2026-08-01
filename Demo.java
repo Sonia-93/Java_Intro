c7f5d11ca0b45f821f5268002fb5dfdda947aa31
Wateta <shimirwasonia5@gmail.com>
2026-08-02 00:00:24 +0200

    update Buffer.class, Demo.class, Demo.java and Mobile.class

--- Demo.java
+++ Demo.java
@@ -3,10 +3,14 @@ class Mobile{
    String brand;
    int price;
    //static helper variable is used to share the value of a variable with all objects of the class
-    static String name;
+    String name;
     
     public void show(){
       System.out.println(brand+" "+price+" "+name);  
+    }
+
+    public static  void show1(Mobile obj){
+         System.out.println(obj.brand+" "+obj.price+" "+obj.name); 
     }
    
 }
@@ -17,8 +21,13 @@ public class Demo{
 obj1.brand="Apple";
 obj1.price=50000;
 Mobile.name="iPhone";
-   
+obj1.show();
+//Mobile.show();
+ Mobile obj2=new Mobile();
+   obj2.brand="Samsung";
+obj2.price=500;
+obj2.show();
 
-
+Mobile.show1(obj1);
     }
 }
---
+++ Enc.java
@@ -0,0 +1,49 @@
+
+class Human{
+private int age;
+private String name;
+//constructor should have no return type  and the same name as the class
+//default constructor
+public Human(){
+    age=12;
+    name="sonia";
+}
+//Parametarized constructor
+public Human(int a, String n){
+age=a;
+name=n;
+}
+
+public int getAge(){
+    return age;
+}
+
+public void setAge(int age){
+ this.age=age;//this represents the current object and refering to instance variable of that onject
+}
+public void setName(String name){
+ this.name=name;
+}
+
+public String getName(){
+    return name;
+}
+}
+public class Enc{
+    
+    public static void main(String a[]){
+
+  Human obj=new Human();
+   Human obj1=new Human( 18,"sonia");
+  
+   //obj.setName("sonia");
+ // obj.setAge(11);
+  System.out.println(obj.getName());
+  System.out.println(obj.getAge());
+
+   System.out.println(obj1.getName());
+  System.out.println(obj1.getAge());
+
+  
+    }
+ 
