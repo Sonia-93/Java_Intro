c538edf13ad660698f9fd93a8dcb492e8da6b33b
Wateta <shimirwasonia5@gmail.com>
2026-08-02 15:03:52 +0200

    update Inher.class and Inher.java

--- Inher.java
+++ Inher.java
@@ -3,11 +3,12 @@ public class Inher{
 
     public static void main(String a[]){
  
-   AdvCalc obj= new AdvCalc();
+   VeryAdvCalc obj= new VeryAdvCalc();
    int r1=obj.add(2,4);
    int r2=obj.sub(9,3);
    int r3=obj.multi(2,4);
    int r4=obj.div(9,3);
-   System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+   double r5=obj.power(2,3);
+   System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
---
+++ VeryAdvCalc.java
@@ -0,0 +1,6 @@
+public class VeryAdvCalc extends AdvCalc{
+
+    public double power(int a, int b){
+        return (int) Math.pow(a, b);
+    }
+}
