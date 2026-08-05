fa8a43420bf97c12c47f2233d5f80619738c8ff9
Wateta <shimirwasonia5@gmail.com>
2026-08-05 15:53:14 +0200

    update A.class, B.class, Exer.class and Exer.java

--- Exer.java
+++ Exer.java
@@ -3,11 +3,24 @@ interface A{
     void show();
     void config();
 }
+class B implements A{
+     
+     public void show(){
+        System.out.println("In Show");
+     }
+
+     public void config(){
+        System.out.println("In config");   
+     }
+}
+
 public class Exer {
 
      public static void main (String a[]){
    
-    
+     A obj=new B();
+     obj.show();
+     obj.config();
       
     }} 
 
