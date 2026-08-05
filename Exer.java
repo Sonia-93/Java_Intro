3ed259bf29cf7bc31eb16b6e697b03b1129a023b
Wateta <shimirwasonia5@gmail.com>
2026-08-05 08:37:16 +0200

    update Exer.java

--- Exer.java
+++ Exer.java
@@ -1,26 +1,12 @@
-class A{
+class Car{
 
-public void show1(){
-    System.out.println("In A show");
- }
-}
-
-class B extends A{
-public void show2(){
-    System.out.println("In B show");
- }
 }
 
 
 public class Exer {
 
      public static void main (String a[]){
-    //Upcasting
-     A obj= (A) new B();
-     obj.show1();
- 
-    //Downcasting
-    B obj1= (B) obj;
-    obj1.show2();
+   
+
  
     }}
