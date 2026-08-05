449314c30896fee8902b217161038b6d7ef65022
Wateta <shimirwasonia5@gmail.com>
2026-08-05 14:07:08 +0200

    update Exer.java

--- Exer.java
+++ Exer.java
@@ -1,25 +1,12 @@
 
 class A{
-    int age;
-    public void show(){
-        System.out.println("In A show");
-        }
-
-     static class B{
-  public void config(){
-    System.out.println("In B config");
-  }
-     }
+    
 }
 public class Exer {
 
      public static void main (String a[]){
    
-    A obj= new A();
-    obj.show();
-      
-      A.B obj1= new A.B();
-      obj1.config();
+
       
     }} 
 
