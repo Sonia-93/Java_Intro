4d8e1a30c0653404848c9a5be1c023222d48f8ff
Wateta <shimirwasonia5@gmail.com>
2026-08-05 13:42:22 +0200

    update Exer.java

--- Exer.java
+++ Exer.java
@@ -1,29 +1,25 @@
-abstract class Car{
-  
-   public abstract void drive();
 
-
-  public void playMusic(){
- System.out.println("Playing Music");
-  }
-
-}
-class BMW extends Car{
-  public void drive(){
-    System.out.println("Driving BMW");
-  }
-}
 
 public class Exer {
 
      public static void main (String a[]){
    
-    Car obj= new BMW();
+  
+      
+
+    }}
+
+
+
+
+
+
+
+
+
+ /* Car obj= new BMW();
     obj.drive();
     obj.playMusic();
     //example of downcasting
     BMW obj1=(BMW)obj;
-    obj1.drive();
-      
-      
-    }}
+    obj1.drive();*/
