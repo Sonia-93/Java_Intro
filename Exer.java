b4d9b93e4d777c8ceb27ccd674bdb6d01faf6465
Wateta <shimirwasonia5@gmail.com>
2026-08-05 07:36:48 +0200

    update A.class, B.class, Exer.class and Exer.java

--- Exer.java
+++ Exer.java
@@ -1,10 +1,24 @@
+class A{
 
+public void show1(){
+    System.out.println("In A show");
+ }
+}
+
+class B extends A{
+public void show2(){
+    System.out.println("In B show");
+ }
+}
 
 
 public class Exer {
 
      public static void main (String a[]){
-    
-    
+    //Upcasting
+     A obj= (A) new B();
+     obj.show1();
+ 
+ 
     }
 }
