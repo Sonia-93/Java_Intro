1fcbeece65882b29e8328356d034ccc3f7d4785c
Wateta <shimirwasonia5@gmail.com>
2026-08-05 07:28:20 +0200

    update Exer.java

--- Exer.java
+++ Exer.java
@@ -1,20 +1,9 @@
 
-class Laptop   {
 
- int price;
-  String brand; 
-
-  public String toString(){
-    return "Hey";
-  }
-}
 
 public class Exer {
 
      public static void main (String a[]){
-     Laptop obj= new Laptop();
-     obj. price=2000;
-     obj.brand="Apple";
- System.out.println(obj);
+    
     }
 }
